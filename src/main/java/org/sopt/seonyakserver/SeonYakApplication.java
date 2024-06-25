047cf85f78f95dbdd6deb64aa2b85ff74fb212a9
HongGit <soojam@konkuk.ac.kr>
2024-06-26 00:42:57 +0900

    Initial commit

---
+++ src/main/java/org/sopt/seonyakserver/SeonYakApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.seonyakserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeonYakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeonYakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sopt/seonyakserver/SeonYakApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.seonyakserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeonYakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
