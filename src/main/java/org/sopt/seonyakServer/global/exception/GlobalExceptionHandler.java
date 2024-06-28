14ea279a1333dff793c34eea6aa70aa1a3c79a83
HongGit <soojam@konkuk.ac.kr>
2024-06-28 21:55:54 +0900

    [SETTING/#4] 커스텀 에러, 핸들러 설정

--- src/main/java/org/sopt/seonyakServer/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/seonyakServer/global/exception/GlobalExceptionHandler.java
@@ -1,4 +1,21 @@
 package org.sopt.seonyakServer.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.sopt.seonyakServer.global.exception.enums.ErrorType;
+import org.sopt.seonyakServer.global.exception.model.CustomException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    // 비즈니스 로직에서 발생한 예외 (언체크)
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorType> handleBusinessException(CustomException e) {
+        log.error("GlobalExceptionHandler catch CustomException : {}", e.getErrorType().getMessage());
+        return ResponseEntity
+                .status(e.getErrorType().getHttpStatus())
+                .body(e.getErrorType());
+    }
 }
---
+++ src/main/java/org/sopt/seonyakServer/global/exception/model/CustomException.java
@@ -0,0 +1,18 @@
+package org.sopt.seonyakServer.global.exception.model;
+
+import lombok.Getter;
+import org.sopt.seonyakServer.global.exception.enums.ErrorType;
+
+@Getter
+public class CustomException extends RuntimeException{
+    private final ErrorType errorType;
+
+    public CustomException(ErrorType errorType) {
+        super(errorType.getMessage());
+        this.errorType = errorType;
+    }
+
+    public int getHttpStatus() {
+        return errorType.getHttpStatusCode();
+    }
+}
