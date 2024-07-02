c402da545d3485bbae24d0b995aabcecf7e08fbc
HongGit <soojam@konkuk.ac.kr>
2024-07-02 18:55:57 +0900

    [FEAT/#8] OCR 요청 컨트롤러 구현

---
+++ src/main/java/org/sopt/seonyakServer/global/common/external/naver/OcrController.java
@@ -0,0 +1,24 @@
+package org.sopt.seonyakServer.global.common.external.naver;
+
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.sopt.seonyakServer.global.common.dto.OcrTextResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class OcrController {
+
+    private final OcrService ocrService;
+
+    @PostMapping("/ocr")
+    public ResponseEntity<OcrTextResponse> ocrText(@RequestParam("imageFile") MultipartFile file) throws IOException {
+        return ResponseEntity.ok(ocrService.ocrText(file));
+    }
+}
