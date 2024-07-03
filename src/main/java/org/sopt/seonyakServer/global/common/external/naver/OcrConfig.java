00186bd7ac49c210ff9f1730fb35d010188d9b5a
HongGit <soojam@konkuk.ac.kr>
2024-07-03 18:19:57 +0900

    [FIX/#8] OCR 설정파일에 Getter 적용

--- src/main/java/org/sopt/seonyakServer/global/common/external/naver/OcrConfig.java
+++ src/main/java/org/sopt/seonyakServer/global/common/external/naver/OcrConfig.java
@@ -1,22 +1,17 @@
 package org.sopt.seonyakServer.global.common.external.naver;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Getter
 public class OcrConfig {
-    
+
     @Value("${naver.ocr.api.url}")
     private String apiUrl;
 
     @Value("${naver.ocr.api.key}")
     private String apiKey;
-
-    public String getApiUrl() {
-        return apiUrl;
-    }
-
-    public String getApiKey() {
-        return apiKey;
-    }
+    
 }
