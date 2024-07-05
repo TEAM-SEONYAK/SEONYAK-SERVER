cf43e48af68b6f5d883cb0c515cf828d638b0856
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-06 04:18:29 +0900

    [REFACTOR/#10] querydsl -> Spring Data JPA의 쿼리 메서드로 변경
    findBySocialTypeAndSocialId

--- src/main/java/org/sopt/seonyakServer/domain/member/repository/MemberRepository.java
+++ src/main/java/org/sopt/seonyakServer/domain/member/repository/MemberRepository.java
@@ -1,7 +1,10 @@
 package org.sopt.seonyakServer.domain.member.repository;
 
+import java.util.Optional;
 import org.sopt.seonyakServer.domain.member.model.Member;
+import org.sopt.seonyakServer.global.common.external.client.SocialType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
 }
