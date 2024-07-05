613353b6c4d45c98983dc299f6e35625bd8504fa
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-06 00:27:16 +0900

    [FEAT/#10] 예외 처리 추가

--- src/main/java/org/sopt/seonyakServer/global/exception/enums/ErrorType.java
+++ src/main/java/org/sopt/seonyakServer/global/exception/enums/ErrorType.java
@@ -8,25 +8,27 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ErrorType {
+
     /**
      * 400 BAD REQUEST
      */
-
     // 표준 오류
-    REQUEST_VALIDATION_ERROR(HttpStatus.BAD_REQUEST,"40001", "잘못된 요청입니다."),
-    INVALID_TYPE_ERROR(HttpStatus.BAD_REQUEST,"40002","잘못된 타입이 입력되었습니다."),
-    INVALID_MISSING_HEADER_ERROR(HttpStatus.BAD_REQUEST,"40003", "요청에 필요한 헤더값이 존재하지 않습니다."),
+    REQUEST_VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "40001", "잘못된 요청입니다."),
+    INVALID_TYPE_ERROR(HttpStatus.BAD_REQUEST, "40002", "잘못된 타입이 입력되었습니다."),
+    INVALID_MISSING_HEADER_ERROR(HttpStatus.BAD_REQUEST, "40003", "요청에 필요한 헤더값이 존재하지 않습니다."),
     INVALID_HTTP_REQUEST_ERROR(HttpStatus.BAD_REQUEST, "40004", "요청 형식이 허용된 형식과 다릅니다."),
     INVALID_HTTP_METHOD_ERROR(HttpStatus.BAD_REQUEST, "40005", "지원되지 않는 HTTP method 요청입니다."),
     INVALID_TOKEN_HEADER_ERROR(HttpStatus.BAD_REQUEST, "40006", "토큰 헤더값의 형식이 잘못되었습니다."),
     INVALID_CODE_HEADER_ERROR(HttpStatus.BAD_REQUEST, "40007", "code 헤더값의 형식이 잘못되었습니다."),
+    INVALID_SOCIAL_TYPE_ERROR(HttpStatus.BAD_REQUEST, "40008", "유효하지 않은 Social Type입니다."),
+    BEARER_LOST_ERROR(HttpStatus.BAD_REQUEST, "40009", "요청한 토큰이 Bearer 토큰이 아닙니다."),
 
     // S3 관련 오류
-    IMAGE_EXTENSION_ERROR(HttpStatus.BAD_REQUEST, "40008", "이미지 확장자는 jpg, png, webp만 가능합니다."),
-    IMAGE_SIZE_ERROR(HttpStatus.BAD_REQUEST, "40009", "이미지 사이즈는 5MB를 넘을 수 없습니다."),
+    IMAGE_EXTENSION_ERROR(HttpStatus.BAD_REQUEST, "40051", "이미지 확장자는 jpg, png, webp만 가능합니다."),
+    IMAGE_SIZE_ERROR(HttpStatus.BAD_REQUEST, "40052", "이미지 사이즈는 5MB를 넘을 수 없습니다."),
 
     // 인증 관련 오류
-    EMPTY_PRINCIPLE_ERROR(HttpStatus.BAD_REQUEST, "40010", "Principle 객체가 없습니다. (null)"),
+    EMPTY_PRINCIPLE_ERROR(HttpStatus.BAD_REQUEST, "40076", "Principle 객체가 없습니다. (null)"),
 
     /**
      * 401 UNAUTHORIZED
@@ -39,7 +41,8 @@ public enum ErrorType {
     UNKNOWN_JWT_ERROR(HttpStatus.UNAUTHORIZED, "40106", "알 수 없는 JWT 토큰 오류가 발생했습니다."),
 
     INVALID_SOCIAL_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "40107", "유효하지 않은 소셜 엑세스 토큰입니다."),
-    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "40108", "유효하지 않은 리프레시 토큰입니다, 다시 로그인을 해주세요."),
+    EXPIRED_AUTHENTICATION_CODE(HttpStatus.UNAUTHORIZED, "40108", "인가 코드가 만료되었습니다."),
+    UN_LOGIN_ERROR(HttpStatus.UNAUTHORIZED, "40109", "로그인 후 진행해주세요."),
 
     /**
      * 404 NOT FOUND
@@ -50,8 +53,7 @@ public enum ErrorType {
     /**
      * 409 CONFLICT
      */
-    NICKNAME_DUP_ERROR(HttpStatus.CONFLICT, "40901", "중복된 회원 닉네임입니다."),
-
+    NICKNAME_DUP_ERROR(HttpStatus.CONFLICT, "40901", "이미 사용 중인 닉네임입니다."),
 
     /**
      * 500 INTERNAL SERVER ERROR
