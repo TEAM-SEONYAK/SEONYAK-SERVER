3dc488ac77e84962e6fed16d462fafeedfcd6909
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-06 00:30:15 +0900

    [FEAT/#10] Member Controller 구현

---
+++ src/main/java/org/sopt/seonyakServer/domain/member/controller/MemberController.java
@@ -0,0 +1,31 @@
+package org.sopt.seonyakServer.domain.member.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.sopt.seonyakServer.domain.member.dto.LoginSuccessResponse;
+import org.sopt.seonyakServer.domain.member.service.MemberService;
+import org.sopt.seonyakServer.global.common.dto.ResponseDto;
+import org.sopt.seonyakServer.global.common.external.client.dto.MemberLoginRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/login")
+    public ResponseDto<LoginSuccessResponse> login(
+            @RequestParam final String authorizationCode,
+            @RequestBody @Valid final MemberLoginRequest loginRequest
+    ) {
+        LoginSuccessResponse loginSuccessResponse = memberService.create(authorizationCode, loginRequest);
+
+        return ResponseDto.success(loginSuccessResponse);
+    }
+}
