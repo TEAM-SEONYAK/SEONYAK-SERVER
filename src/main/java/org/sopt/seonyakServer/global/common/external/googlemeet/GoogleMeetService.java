1c27b3b59ec99167e1b42cdaea85d8dba47880f5
HongGit <soojam@konkuk.ac.kr>
2024-07-09 02:43:47 +0900

    [FEAT/#15] 구글미트 서비스 구현

---
+++ src/main/java/org/sopt/seonyakServer/global/common/external/googlemeet/GoogleMeetService.java
@@ -0,0 +1,29 @@
+package org.sopt.seonyakServer.global.common.external.googlemeet;
+
+import com.google.apps.meet.v2.CreateSpaceRequest;
+import com.google.apps.meet.v2.Space;
+import com.google.apps.meet.v2.SpacesServiceClient;
+import com.google.apps.meet.v2.SpacesServiceSettings;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.sopt.seonyakServer.global.common.external.googlemeet.dto.GoogleMeetUrlResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GoogleMeetService {
+    private final SpacesServiceSettings spacesServiceSettings;
+
+    public GoogleMeetUrlResponse createMeetingSpace() throws Exception {
+        try (SpacesServiceClient spacesServiceClient = SpacesServiceClient.create(spacesServiceSettings)) {
+            CreateSpaceRequest request = CreateSpaceRequest.newBuilder()
+                    .setSpace(Space.newBuilder().build())
+                    .build();
+            Space response = spacesServiceClient.createSpace(request);
+            return GoogleMeetUrlResponse.of(response.getMeetingUri());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
--- src/main/java/org/sopt/seonyakServer/global/exception/enums/ErrorType.java
+++ src/main/java/org/sopt/seonyakServer/global/exception/enums/ErrorType.java
@@ -59,7 +59,8 @@ public enum ErrorType {
      * 500 INTERNAL SERVER ERROR
      */
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "50001", "알 수 없는 서버 에러가 발생했습니다."),
-    GET_UPLOAD_PRESIGNED_URL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "50002", "업로드를 위한 Presigned URL 획득에 실패했습니다.");
+    GET_UPLOAD_PRESIGNED_URL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "50002", "업로드를 위한 Presigned URL 획득에 실패했습니다."),
+    GET_GOOGLE_MEET_URL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "50003", "구글미트 URL 획득에 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
