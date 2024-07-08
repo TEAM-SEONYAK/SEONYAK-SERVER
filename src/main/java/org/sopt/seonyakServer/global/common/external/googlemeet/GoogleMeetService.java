640dd3d41867ff171466f23851b0061d6cbb607b
HongGit <soojam@konkuk.ac.kr>
2024-07-09 03:34:09 +0900

    [DEL/#15] 불필요한 try-catch문 삭제

--- src/main/java/org/sopt/seonyakServer/global/common/external/googlemeet/GoogleMeetService.java
+++ src/main/java/org/sopt/seonyakServer/global/common/external/googlemeet/GoogleMeetService.java
@@ -4,7 +4,6 @@ import com.google.apps.meet.v2.CreateSpaceRequest;
 import com.google.apps.meet.v2.Space;
 import com.google.apps.meet.v2.SpacesServiceClient;
 import com.google.apps.meet.v2.SpacesServiceSettings;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.sopt.seonyakServer.global.common.external.googlemeet.dto.GoogleMeetUrlResponse;
 import org.springframework.stereotype.Service;
@@ -15,15 +14,11 @@ public class GoogleMeetService {
     private final SpacesServiceSettings spacesServiceSettings;
 
     public GoogleMeetUrlResponse createMeetingSpace() throws Exception {
-        try (SpacesServiceClient spacesServiceClient = SpacesServiceClient.create(spacesServiceSettings)) {
-            CreateSpaceRequest request = CreateSpaceRequest.newBuilder()
-                    .setSpace(Space.newBuilder().build())
-                    .build();
-            Space response = spacesServiceClient.createSpace(request);
-            return GoogleMeetUrlResponse.of(response.getMeetingUri());
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw e;
-        }
+        SpacesServiceClient spacesServiceClient = SpacesServiceClient.create(spacesServiceSettings);
+        CreateSpaceRequest request = CreateSpaceRequest.newBuilder()
+                .setSpace(Space.newBuilder().build())
+                .build();
+        Space response = spacesServiceClient.createSpace(request);
+        return GoogleMeetUrlResponse.of(response.getMeetingUri());
     }
 }
