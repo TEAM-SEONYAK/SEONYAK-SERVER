49cfbca30cdeae9d66beb031207dd26b063a1ae0
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-09 19:20:32 +0900

    [FEAT/#20] Senior 엔티티 작성

---
+++ src/main/java/org/sopt/seonyakServer/domain/senior/model/Senior.java
@@ -0,0 +1,68 @@
+package org.sopt.seonyakServer.domain.senior.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.seonyakServer.domain.member.model.Member;
+import org.sopt.seonyakServer.domain.util.JsonConverter;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "senior")
+public class Senior {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "senior_id")
+    private Long seniorId;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "member_id", referencedColumnName = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "business_card", nullable = false, length = 255)
+    private String businessCard;
+
+    @Column(name = "company", length = 255)
+    private String company;
+
+    @Column(name = "position", length = 255)
+    private String position;
+
+    @Column(name = "detail_position", nullable = false, length = 255)
+    private String detailPosition;
+
+    @Column(name = "level", nullable = false)
+    private Integer level;
+
+    @Column(name = "catchphrase", length = 255)
+    private String catchphrase;
+
+    @Column(name = "career", length = 255)
+    private String career;
+
+    @Column(name = "award", length = 255)
+    private String award;
+
+    @Column(name = "story", length = 255)
+    private String story;
+
+    @Column(name = "preffered_time_list", columnDefinition = "jsonb")
+    @Convert(converter = JsonConverter.class)
+    private Map<String, Object> prefferedTimeList;
+}
