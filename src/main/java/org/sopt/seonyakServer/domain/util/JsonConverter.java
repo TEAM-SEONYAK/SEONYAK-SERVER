54292e77fc91ecfd64d797659596280b30763f3d
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-09 19:16:26 +0900

    [FEAT/#20] jsonb 사용을 위한 JsonConverter 추가

---
+++ src/main/java/org/sopt/seonyakServer/domain/util/JsonConverter.java
@@ -0,0 +1,33 @@
+package org.sopt.seonyakServer.domain.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Converter
+public class JsonConverter implements AttributeConverter<Map<String, Object>, String> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> attribute) {
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error converting map to JSON string.", e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> convertToEntityAttribute(String dbData) {
+        try {
+            return objectMapper.readValue(dbData, HashMap.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Error converting JSON string to map.", e);
+        }
+    }
+}
