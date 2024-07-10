fb495a8211a55d0009ec87ba049b65bfbd0ca794
HongGit <soojam@konkuk.ac.kr>
2024-07-11 01:45:56 +0900

    [CHORE/#55] 대학명, 명함 OCR 설정값 분리

--- src/main/java/org/sopt/seonyakServer/global/auth/security/SecurityConfig.java
+++ src/main/java/org/sopt/seonyakServer/global/auth/security/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
 
     private static final String[] AUTH_WHITE_LIST = {
             "/api/v1/**",
+            "/api/v1/ocr/**",
             "/api/v1/auth/**",
             "/actuator/health",
             "/v3/api-docs/**",
--- src/main/java/org/sopt/seonyakServer/global/common/external/naver/OcrConfig.java
+++ src/main/java/org/sopt/seonyakServer/global/common/external/naver/OcrConfig.java
@@ -8,10 +8,16 @@ import org.springframework.context.annotation.Configuration;
 @Getter
 public class OcrConfig {
 
-    @Value("${naver.ocr.api.url}")
-    private String apiUrl;
+    @Value("${naver.ocr.api.univ-url}")
+    private String univUrl;
 
-    @Value("${naver.ocr.api.key}")
-    private String apiKey;
-    
+    @Value("${naver.ocr.api.univ-key}")
+    private String univUrlKey;
+
+    @Value("${naver.ocr.api.business-url}")
+    private String businessUrl;
+
+    @Value("${naver.ocr.api.business-key}")
+    private String businessKey;
+
 }
