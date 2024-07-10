4d5f4d48d97a6d29731f8f19952fb2538b98fb87
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-11 08:43:13 +0900

    [FEAT/#61] 닉네임 유효성 검증 엔드포인트 구현

--- src/main/java/org/sopt/seonyakServer/domain/member/controller/MemberController.java
+++ src/main/java/org/sopt/seonyakServer/domain/member/controller/MemberController.java
@@ -3,7 +3,9 @@ package org.sopt.seonyakServer.domain.member.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.sopt.seonyakServer.domain.member.dto.LoginSuccessResponse;
+import org.sopt.seonyakServer.domain.member.dto.NicknameRequest;
 import org.sopt.seonyakServer.domain.member.service.MemberService;
+import org.sopt.seonyakServer.global.common.dto.ResponseDto;
 import org.sopt.seonyakServer.global.common.external.client.dto.MemberLoginRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,16 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/login")
+    @PostMapping("/auth/login")
     public ResponseEntity<LoginSuccessResponse> login(
             @RequestParam final String authorizationCode,
             @RequestBody @Valid final MemberLoginRequest loginRequest
     ) {
         return ResponseEntity.ok(memberService.create(authorizationCode, loginRequest));
+    }
+
+    @PostMapping("/nickname")
+    public ResponseDto<?> validNickname(
+            @RequestBody final NicknameRequest nicknameRequest
+    ) {
+        memberService.validNickname(nicknameRequest);
+
+        return ResponseDto.success(null);
     }
 }
