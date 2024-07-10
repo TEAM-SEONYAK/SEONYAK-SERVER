ca1113afc5f5331e7181bfd41a63c11b784f6205
HongGit <soojam@konkuk.ac.kr>
2024-07-11 05:47:34 +0900

    [CHORE/#59] Presigned  URL 설정 추가

--- src/main/java/org/sopt/seonyakServer/global/config/S3Config.java
+++ src/main/java/org/sopt/seonyakServer/global/config/S3Config.java
@@ -7,10 +7,11 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
-    
+
     @Value("${aws-property.access-key}")
     private String accessKeyId;
 
@@ -23,6 +24,16 @@ public class S3Config {
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
+                .region(Region.of(awsRegion))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKeyId, secretAccessKey)
+                ))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner getS3Presigner() {
+        return S3Presigner.builder()
                 .region(Region.of(awsRegion))
                 .credentialsProvider(StaticCredentialsProvider.create(
                         AwsBasicCredentials.create(accessKeyId, secretAccessKey)
