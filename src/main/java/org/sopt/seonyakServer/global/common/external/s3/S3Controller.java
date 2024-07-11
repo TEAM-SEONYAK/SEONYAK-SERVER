a5ddd5fd9823cf68eb3c632628844728142b9ebd
HongGit <soojam@konkuk.ac.kr>
2024-07-11 23:12:56 +0900

    [FIX/#59] Presigned URL 요청 경로 수정

--- src/main/java/org/sopt/seonyakServer/global/common/external/s3/S3Controller.java
+++ src/main/java/org/sopt/seonyakServer/global/common/external/s3/S3Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class S3Controller {
     private final S3Service s3Service;
