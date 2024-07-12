e57b939669d59c789b60d7e83634ffa95b1ab3d1
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-13 07:16:48 +0900

    [FIX/#66] NicknameRequest 빈 값일 때도 200 OK 뜨는 오류 해결

--- src/main/java/org/sopt/seonyakServer/domain/member/dto/NicknameRequest.java
+++ src/main/java/org/sopt/seonyakServer/domain/member/dto/NicknameRequest.java
@@ -1,6 +1,9 @@
 package org.sopt.seonyakServer.domain.member.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record NicknameRequest(
+        @NotBlank(message = "닉네임은 공백일 수 없습니다.")
         String nickname
 ) {
 }
