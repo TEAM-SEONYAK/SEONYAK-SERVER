c53668cc617c822f6ada9a265e0a4b0e59323052
HongGit <soojam@konkuk.ac.kr>
2024-07-13 22:36:16 +0900

    [FEAT/#79] 약속 수락/거절 컨트롤러 구현

--- src/main/java/org/sopt/seonyakServer/domain/appointment/controller/AppointmentController.java
+++ src/main/java/org/sopt/seonyakServer/domain/appointment/controller/AppointmentController.java
@@ -2,9 +2,12 @@ package org.sopt.seonyakServer.domain.appointment.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.seonyakServer.domain.appointment.dto.AppointmentAcceptRequest;
+import org.sopt.seonyakServer.domain.appointment.dto.AppointmentRejectRequest;
 import org.sopt.seonyakServer.domain.appointment.dto.AppointmentRequest;
 import org.sopt.seonyakServer.domain.appointment.service.AppointmentService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,22 @@ public class AppointmentController {
             @RequestBody final AppointmentRequest appointmentRequest
     ) {
         appointmentService.postAppointment(appointmentRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/accept")
+    public ResponseEntity<Void> acceptAppointment(
+            @RequestBody AppointmentAcceptRequest appointmentAcceptRequest
+    ) {
+        appointmentService.acceptAppointment(appointmentAcceptRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/reject")
+    public ResponseEntity<Void> rejectAppointment(
+            @RequestBody AppointmentRejectRequest appointmentRejectRequest
+    ) {
+        appointmentService.rejectAppointment(appointmentRejectRequest);
         return ResponseEntity.ok().build();
     }
 }
