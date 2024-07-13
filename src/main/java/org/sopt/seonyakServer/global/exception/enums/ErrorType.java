9f6b45b46cf63239ce0e1269a9b5ccfdd7493439
HongGit <soojam@konkuk.ac.kr>
2024-07-13 21:10:28 +0900

    [FEAT/#76] 자기자신에게 약속 신청 에러 추가

--- src/main/java/org/sopt/seonyakServer/global/exception/enums/ErrorType.java
+++ src/main/java/org/sopt/seonyakServer/global/exception/enums/ErrorType.java
@@ -31,6 +31,7 @@ public enum ErrorType {
     MAP_TO_JSON_ERROR(HttpStatus.BAD_REQUEST, "40016", "Map을 JSON 문자열로 변환하는 중 오류가 발생했습니다."),
     JSON_TO_MAP_ERROR(HttpStatus.BAD_REQUEST, "40017", "JSON 문자열을 Map으로 변환하는 중 오류가 발생했습니다."),
     UNIV_CERT_REQUEST_ERROR(HttpStatus.BAD_REQUEST, "40018", "이미 인증이 완료 이메일입니다."),
+    SAME_MEMBER_APPOINTMENT_ERROR(HttpStatus.BAD_REQUEST, "40019", "자기 자신에게는 약속을 신청할 수 없습니다."),
 
     // S3 관련 오류
     IMAGE_EXTENSION_ERROR(HttpStatus.BAD_REQUEST, "40051", "이미지 확장자는 jpg, png, webp만 가능합니다."),
