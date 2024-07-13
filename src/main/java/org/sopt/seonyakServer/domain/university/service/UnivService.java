6f766897cbb16e73e6078a2e6f3e60c5ea625969
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-13 22:39:24 +0900

    [FEAT/#77] 학과 검색 서비스 로직 구현

--- src/main/java/org/sopt/seonyakServer/domain/university/service/UnivService.java
+++ src/main/java/org/sopt/seonyakServer/domain/university/service/UnivService.java
@@ -1,7 +1,12 @@
 package org.sopt.seonyakServer.domain.university.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.sopt.seonyakServer.domain.university.dto.SearchDeptResponse;
 import org.sopt.seonyakServer.domain.university.dto.SearchUnivResponse;
+import org.sopt.seonyakServer.domain.university.model.Department;
+import org.sopt.seonyakServer.domain.university.repository.DeptRepository;
 import org.sopt.seonyakServer.domain.university.repository.UnivRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +15,23 @@ import org.springframework.stereotype.Service;
 public class UnivService {
 
     private final UnivRepository univRepository;
+    private final DeptRepository deptRepository;
 
-    public SearchUnivResponse searchUniv(String univNamePart) {
+    public SearchUnivResponse searchUniv(final String univNamePart) {
         return SearchUnivResponse.of(univRepository.findByUnivNameContaining(univNamePart));
+    }
+
+    public List<SearchDeptResponse> searchDept(
+            final String univName,
+            final String deptName
+    ) {
+        List<Department> departments = deptRepository.findByUnivIdAndDeptNameContaining(univName, deptName);
+
+        return departments.stream()
+                .map(department -> SearchDeptResponse.of(
+                        department.getDeptName(),
+                        department.isClosed())
+                )
+                .collect(Collectors.toList());
     }
 }
