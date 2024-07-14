6f5e3e5c369e30c961e06722f416e8fa9e7283b9
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-14 12:12:29 +0900

    [FEAT/#88] AppointmentRepository JPA 메서드 추가

--- src/main/java/org/sopt/seonyakServer/domain/appointment/repository/AppointmentRepository.java
+++ src/main/java/org/sopt/seonyakServer/domain/appointment/repository/AppointmentRepository.java
@@ -1,7 +1,29 @@
 package org.sopt.seonyakServer.domain.appointment.repository;
 
+import java.util.Optional;
 import org.sopt.seonyakServer.domain.appointment.model.Appointment;
+import org.sopt.seonyakServer.global.exception.enums.ErrorType;
+import org.sopt.seonyakServer.global.exception.model.CustomException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    Optional<Appointment> findById(Long appointmentId);
+
+    default Appointment findAppointmentByIdOrThrow(Long appointmentId) {
+        return findById(appointmentId)
+                .orElseThrow(() -> new CustomException(ErrorType.NOT_FOUND_APPOINTMENT_ERROR));
+    }
+
+    @Query("SELECT a.member.id "
+            + "FROM Appointment a "
+            + "WHERE a.id = :appointmentId")
+    Long findMemberIdById(@Param("appointmentId") Long appointmentId);
+
+    @Query("SELECT a.senior.member.id "
+            + "FROM Appointment a "
+            + "WHERE a.id = :appointmentId")
+    Long findSeniorIdById(@Param("appointmentId") Long appointmentId);
 }
