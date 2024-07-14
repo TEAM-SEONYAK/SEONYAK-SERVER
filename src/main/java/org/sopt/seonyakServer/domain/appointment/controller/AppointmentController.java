f0e72f5733e5f2fdc014ce16b57d14c3e2eac6f7
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-14 12:12:58 +0900

    [FEAT/#88] 약속 상세 페이지 조회 엔드포인트 구현

--- src/main/java/org/sopt/seonyakServer/domain/appointment/controller/AppointmentController.java
+++ src/main/java/org/sopt/seonyakServer/domain/appointment/controller/AppointmentController.java
@@ -2,9 +2,12 @@ package org.sopt.seonyakServer.domain.appointment.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.seonyakServer.domain.appointment.dto.AppointmentDetailRequest;
+import org.sopt.seonyakServer.domain.appointment.dto.AppointmentDetailResponse;
 import org.sopt.seonyakServer.domain.appointment.dto.AppointmentRequest;
 import org.sopt.seonyakServer.domain.appointment.service.AppointmentService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/appoinment")
+@RequestMapping("/api/v1/appointment")
 public class AppointmentController {
 
     private final AppointmentService appointmentService;
@@ -23,5 +26,12 @@ public class AppointmentController {
     ) {
         appointmentService.postAppointment(appointmentRequest);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/detail")
+    public ResponseEntity<AppointmentDetailResponse> getAppointmentDetail(
+            @RequestBody final AppointmentDetailRequest appointmentDetailRequest
+    ) {
+        return ResponseEntity.ok(appointmentService.getAppointmentDetail(appointmentDetailRequest));
     }
 }
