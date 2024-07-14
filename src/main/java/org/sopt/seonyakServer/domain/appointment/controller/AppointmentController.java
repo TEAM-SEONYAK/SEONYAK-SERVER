93dd0dd33401d29a50be04a5d97cfc34eab35656
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-14 09:02:34 +0900

    [FEAT/#84] 구글 미트 회의실 링크 조회 엔드포인트 구현

--- src/main/java/org/sopt/seonyakServer/domain/appointment/controller/AppointmentController.java
+++ src/main/java/org/sopt/seonyakServer/domain/appointment/controller/AppointmentController.java
@@ -3,8 +3,11 @@ package org.sopt.seonyakServer.domain.appointment.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.seonyakServer.domain.appointment.dto.AppointmentRequest;
+import org.sopt.seonyakServer.domain.appointment.dto.GoogleMeetLinkRequest;
+import org.sopt.seonyakServer.domain.appointment.dto.GoogleMeetLinkResponse;
 import org.sopt.seonyakServer.domain.appointment.service.AppointmentService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/appoinment")
+@RequestMapping("/api/v1")
 public class AppointmentController {
 
     private final AppointmentService appointmentService;
 
-    @PostMapping("")
+    @PostMapping("/appoinment")
     public ResponseEntity<Void> postAppointment(
             @RequestBody final AppointmentRequest appointmentRequest
     ) {
         appointmentService.postAppointment(appointmentRequest);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/google-meet")
+    public ResponseEntity<GoogleMeetLinkResponse> getGoogleMeetLink(
+            @RequestBody final GoogleMeetLinkRequest googleMeetLinkRequest
+    ) {
+        return ResponseEntity.ok(appointmentService.getGoogleMeetLink(googleMeetLinkRequest));
     }
 }
