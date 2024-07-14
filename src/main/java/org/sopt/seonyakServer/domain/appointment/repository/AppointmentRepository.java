111bcca117402e559cfebc18247c5a419fd929e4
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-14 09:01:02 +0900

    [FEAT/#84] 구글 미트 링크 관련 JPA 메서드 추가

--- src/main/java/org/sopt/seonyakServer/domain/appointment/repository/AppointmentRepository.java
+++ src/main/java/org/sopt/seonyakServer/domain/appointment/repository/AppointmentRepository.java
@@ -2,6 +2,23 @@ package org.sopt.seonyakServer.domain.appointment.repository;
 
 import org.sopt.seonyakServer.domain.appointment.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    @Query("SELECT a.googleMeetLink "
+            + "FROM Appointment a "
+            + "WHERE a.id = :appointmentId")
+    String findGoogleMeetLinkById(@Param("appointmentId") Long appointmentId);
+
+    @Query("SELECT a.member.id "
+            + "FROM Appointment a "
+            + "WHERE a.id = :appointmentId")
+    Long findMemberIdById(@Param("appointmentId") Long appointmentId);
+
+    @Query("SELECT a.senior.id "
+            + "FROM Appointment a "
+            + "WHERE a.id = :appointmentId")
+    Long findSeniorIdById(@Param("appointmentId") Long appointmentId);
 }
