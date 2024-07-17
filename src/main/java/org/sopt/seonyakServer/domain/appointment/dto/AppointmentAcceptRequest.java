e0fbc730fa5f34cb4d7c77ed68c13d3679a87f6f
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-17 17:02:39 +0900

    [FIX/#101] String이 아닌 필드에 @NotBlank 사용한 오류 수정

--- src/main/java/org/sopt/seonyakServer/domain/appointment/dto/AppointmentAcceptRequest.java
+++ src/main/java/org/sopt/seonyakServer/domain/appointment/dto/AppointmentAcceptRequest.java
@@ -1,15 +1,19 @@
 package org.sopt.seonyakServer.domain.appointment.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 import org.sopt.seonyakServer.domain.appointment.model.DataTimeRange;
 
 public record AppointmentAcceptRequest(
-        @NotBlank(message = "약속 Id는 공백일 수 없습니다.")
+        @NotNull(message = "약속 Id는 공백일 수 없습니다.")
+        @Positive(message = "약속 Id는 양수여야 합니다.")
         Long appointmentId,
         @NotBlank(message = "구글 미트 링크는 공백일 수 없습니다.")
         String googleMeetLink,
-        @NotBlank(message = "timeList는 공백일 수 없습니다.")
+        @NotEmpty(message = "timeList는 공백일 수 없습니다.")
         List<DataTimeRange> timeList
 ) {
 }
--- src/main/java/org/sopt/seonyakServer/domain/appointment/dto/AppointmentRejectRequest.java
+++ src/main/java/org/sopt/seonyakServer/domain/appointment/dto/AppointmentRejectRequest.java
@@ -1,9 +1,12 @@
 package org.sopt.seonyakServer.domain.appointment.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record AppointmentRejectRequest(
-        @NotBlank(message = "약속 Id는 공백일 수 없습니다.")
+        @NotNull(message = "약속 Id는 공백일 수 없습니다.")
+        @Positive(message = "약속 Id는 양수여야 합니다.")
         Long appointmentId,
         @NotBlank(message = "거절 사유는 공백일 수 없습니다.")
         String rejectReason,
