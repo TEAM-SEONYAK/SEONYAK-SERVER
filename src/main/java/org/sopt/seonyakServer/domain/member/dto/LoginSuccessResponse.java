7ba992299028fc35155eab69c242efa3f4a444ce
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-19 08:53:00 +0900

    [FEAT/#121] 소셜 로그인 시 분기 처리용 로직 구현

--- src/main/java/org/sopt/seonyakServer/domain/member/dto/LoginSuccessResponse.java
+++ src/main/java/org/sopt/seonyakServer/domain/member/dto/LoginSuccessResponse.java
@@ -1,9 +1,19 @@
 package org.sopt.seonyakServer.domain.member.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record LoginSuccessResponse(
+        String role,
         String accessToken
 ) {
-    public static LoginSuccessResponse of(final String accessToken) {
-        return new LoginSuccessResponse(accessToken);
+    public static LoginSuccessResponse of(
+            final String role,
+            final String accessToken
+    ) {
+        return new LoginSuccessResponse(
+                role,
+                accessToken
+        );
     }
 }
