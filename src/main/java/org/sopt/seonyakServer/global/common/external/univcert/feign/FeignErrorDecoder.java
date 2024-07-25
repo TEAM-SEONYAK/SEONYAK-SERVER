345a735b31fc909bb474cc0e7dd209c6f1929a32
ckkim817 <ckkim817@konkuk.ac.kr>
2024-07-25 13:44:00 +0900

    [FIX/#140] FeignErrorDecoder 로깅 추가

--- src/main/java/org/sopt/seonyakServer/global/common/external/univcert/feign/FeignErrorDecoder.java
+++ src/main/java/org/sopt/seonyakServer/global/common/external/univcert/feign/FeignErrorDecoder.java
@@ -2,47 +2,54 @@ package org.sopt.seonyakServer.global.common.external.univcert.feign;
 
 import feign.Response;
 import feign.codec.ErrorDecoder;
-import java.io.BufferedReader;
+import feign.codec.StringDecoder;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.sopt.seonyakServer.global.exception.enums.ErrorType;
 import org.sopt.seonyakServer.global.exception.model.CustomException;
 
+@Slf4j
 public class FeignErrorDecoder implements ErrorDecoder {
+
+    private StringDecoder stringDecoder = new StringDecoder();
+
     @Override
     public Exception decode(String methodKey, Response response) {
 
-        // 400에러가 내려오는 경우
-        if (response.status() == 400) {
-            try (InputStream bodyStream = response.body().asInputStream()) {
-                String body = new BufferedReader(new InputStreamReader(bodyStream, StandardCharsets.UTF_8))
-                        .lines().collect(Collectors.joining("\n"));
+        String message;
+
+        if (response.body() != null) {
+            try {
+                String body = (String) stringDecoder.decode(response, String.class);
 
                 // 응답결과 JSON 파싱
                 JSONObject jsonObject = new JSONObject(body);
-                String message = jsonObject.optString("message", "Unknown error");
-
-                // 응답 메시지를 기준으로 분기처리
-                if (message.equals("대학과 일치하지 않는 메일 도메인입니다.")) {
-                    throw new CustomException(ErrorType.INVALID_EMAIL_DOMAIN_ERROR);
-                }
-                if (message.equals("이미 완료된 요청입니다.")) {
-                    throw new CustomException(ErrorType.UNIV_CERT_REQUEST_ERROR);
-                }
-                if (message.equals("인증 요청 이력이 존재하지 않습니다.")) {
-                    throw new CustomException(ErrorType.NO_VERIFICATION_REQUEST_HISTORY);
-                }
-                throw new CustomException(ErrorType.INTERNAL_SERVER_ERROR);
-            } catch (IOException e) {
-                throw new CustomException(ErrorType.INTERNAL_SERVER_ERROR);
+                message = jsonObject.optString("message", "message 필드가 존재하지 않습니다.");
+            } catch (IOException | JSONException e) {
+                log.error(methodKey + "Feign 요청이 실패한 후 받은 Response Body를 객체로 변환하는 과정에서 오류가 발생했습니다.", e);
+                throw new CustomException(ErrorType.INTERNAL_FEIGN_ERROR);
             }
         } else {
-            // 다른 상태 코드에 대한 처리를 추가합니다.
-            return new CustomException(ErrorType.INTERNAL_FEIGN_ERROR);
+            throw new CustomException(ErrorType.NO_RESPONSE_BODY_ERROR);
         }
+
+        // 응답 메시지를 기준으로 분기처리
+        if (message.equals("대학과 일치하지 않는 메일 도메인입니다.")) {
+            throw new CustomException(ErrorType.INVALID_EMAIL_DOMAIN_ERROR);
+        }
+        if (message.equals("이미 완료된 요청입니다.")) {
+            throw new CustomException(ErrorType.UNIV_CERT_REQUEST_ERROR);
+        }
+        if (message.equals("인증 요청 이력이 존재하지 않습니다.")) {
+            throw new CustomException(ErrorType.NO_VERIFICATION_REQUEST_HISTORY);
+        }
+
+        log.error(String.valueOf(response.status()));
+        log.error(message);
+        log.error(String.valueOf(response.headers()));
+
+        throw new CustomException(ErrorType.INTERNAL_SERVER_ERROR);
     }
 }
