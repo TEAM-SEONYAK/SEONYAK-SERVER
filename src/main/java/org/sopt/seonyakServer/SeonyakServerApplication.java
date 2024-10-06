080b68300e0853c5b9398949c14fb46535aca79c
HongGit <soojam@konkuk.ac.kr>
2024-10-07 02:41:45 +0900

    [FEAT/#152] 스웨거 배포 서버 통신을 위한 어노테이션 추가

--- src/main/java/org/sopt/seonyakServer/SeonyakServerApplication.java
+++ src/main/java/org/sopt/seonyakServer/SeonyakServerApplication.java
@@ -1,9 +1,12 @@
 package org.sopt.seonyakServer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://api.seonyak.com", description = "product seonyak server")})
 @SpringBootApplication
 @EnableScheduling
 public class SeonyakServerApplication {
